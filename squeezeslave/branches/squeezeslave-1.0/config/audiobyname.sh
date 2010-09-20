2bc5ca110b6731c8fd408ce9d722e3b3e8ab2178
paddockd <paddockd@2b6bae96-1219-11de-a50a-137a643d546a>
2010-09-20 12:30:59 +0000

    Remove experimental branch

--- squeezeslave/branches/squeezeslave-1.0/config/audiobyname.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Provided by forum member blazerte.
-# http://forums.slimdevices.com/showpost.php?p=443013&postcount=23
-str=$(./squeezeslave -L | grep pulse)
-./squeezeslave -r15 -o ${str%%: pulse}
--- squeezeslave/branches/squeezeslave-1.0/tests/testCurses.mak
+++
@@ -1,3 +0,0 @@
-all:
-	gcc -Wall -O2 testCursesKeypadTrue.c -o testTrue -lcurses
-	gcc -Wall -O2 testCursesKeypadFalse.c -o testFalse -lcurses
