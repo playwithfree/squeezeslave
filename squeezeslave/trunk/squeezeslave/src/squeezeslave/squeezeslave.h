7acc27beac350fd6ec717025b5fa35334ea4dc54
ralph.irving <ralph.irving@2b6bae96-1219-11de-a50a-137a643d546a>
2013-06-01 11:00:48 +0000

    Changed default paFramesPerBuffer to 4096 to prevent portaudio v18.1 write interrupted lockup on linux sun ray oss audio.  Higher values cause utadem driver to throw an out of memory error. Increased revision to 401.

--- squeezeslave/trunk/squeezeslave/src/squeezeslave/squeezeslave.h
+++ squeezeslave/trunk/squeezeslave/src/squeezeslave/squeezeslave.h
@@ -85,7 +85,7 @@
 #else
 #define PLAYER_TYPE	8
 #define FIRMWARE_VERSION	10
-#define PA_FRAMES_PER_BUFFER	8192	/* Sun Ray OSS audio driver emulator (utadem) maximum */
+#define PA_FRAMES_PER_BUFFER	4096	/* Sun Ray OSS audio driver emulator (utadem) maximum */
 #define PA_NUM_BUFFERS		4
 #endif
 
